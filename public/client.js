9186ee72c8efd6dfd008a2be069b9f326b0abd63
Glitch (ced2018) <none>
2018-01-10 21:55:39 +0000

    🌾👼 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,34 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+$(function() {
+  $.getJSON('/data', function(data) {
+    var $dataContainer = $('#data-container');
+      
+    if (data.error) {
+      $dataContainer.html('Error! ' + data.error);
+      return;
+    }
+      
+    // Clear the loading message.
+    $dataContainer.html('');
+      
+    data.records.forEach(function(record) {
+      var $galleryCard = $('<div class="gallery-card" />');
+      if (record.picture[0]) {
+        // Just show the first picture, if it has one.
+        $('<img />').attr('src', record.picture[0].url).appendTo($galleryCard);
+      }
+      var $label = $('<strong />').text(record.name);
+      
+    
+      var $startingtime = $('<p />').text(record.startingtime);
+      
+      $galleryCard.append($label);
+      
+      $galleryCard.append($startingtime);
+      
+      $dataContainer.append($galleryCard);
+    });
+  });
+});
---
+++ server.js
@@ -0,0 +1,64 @@
+// server.js
+// where your node app starts
+
+// We're going to use the "Product Catalog and Orders" base template:
+// https://airtable.com/templates/featured/expZvMLT9L6c4yeBX/product-catalog-and-orders
+var Airtable = require('airtable');
+var base = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_BASE_ID);
+
+
+var tableName ='Events';
+var viewName = 'All Programs';
+
+var express = require('express');
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// Cache the records in case we get a lot of traffic.
+// Otherwise, we'll hit Airtable's rate limit.
+var cacheTimeoutMs = 5 * 1000; // Cache for 5 seconds.
+var cachedResponse = null;
+var cachedResponseDate = null;
+
+app.get("/data", function(_, response) {
+  if (cachedResponse && new Date() - cachedResponseDate < cacheTimeoutMs) {
+    response.send(cachedResponse);
+  } else {
+    // Select the first 10 records from the view.
+    base(tableName).select({
+      maxRecords: 10,
+      view: viewName,
+    }).firstPage(function(error, records) {
+      if (error) {
+        response.send({error: error});
+      } else {
+        cachedResponse = {
+          records: records.map(record => {
+            return {
+              name: record.get('Program'),
+              picture: record.get('Pic'),
+              startingtime: record.get('Starting Time Formatted'),
+            };
+          }),
+        };
+        cachedResponseDate = new Date();
+        
+        response.send(cachedResponse);
+      }
+    });
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
